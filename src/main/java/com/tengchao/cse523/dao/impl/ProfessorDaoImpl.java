767837fd9e037637ef37a2aa2f2a2b90f584a552
tengchao <tengchao@localhost>
2015-03-25 21:24:55 -0700

    1. clean unused packages 2. clean unused imports

--- src/main/java/com/tengchao/cse523/dao/impl/ProfessorDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package com.tengchao.cse523.dao.impl;
-
-import com.tengchao.cse523.dao.BaseDao;
-import com.tengchao.cse523.dao.ProfessorDao;
-
-public class ProfessorDaoImpl extends BaseDao implements ProfessorDao {
-
-
-}
